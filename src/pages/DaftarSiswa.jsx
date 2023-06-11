65837f6ba4837a5a932dae72cf94314c71c2bb7e
ipint <ma7804058@gmail.com>
2023-06-11 10:28:41 +0700

    add readme

--- src/pages/DaftarSiswa.jsx
+++ src/pages/DaftarSiswa.jsx
@@ -8,7 +8,7 @@ const DaftarSiswa = () => {
       <Navbar />
       <div className="bg-white min-h-screen mx-40">
         <header className="bg-white pt-14">
-          <h1 className="text-2xl font-bold">My Student App</h1>
+          <h1 className="text-2xl font-bold">Student App</h1>
           <p className="text-gray-500 mt-4">Refresh halaman jika data belom muncul!!</p>
         </header>    
         <main className="py-8">
